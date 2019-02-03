564ef6e1fc67ed6585f5b0311b458738eebcc6bf
Musa <halilovmusa@gmail.com>
2019-02-03 22:09:46 +0300

    Добавлено ограничение на количество попыток угадать число

--- main.cpp
+++ main.cpp
@@ -7,21 +7,25 @@ int generate_number(int start, int finish);
 
 int main()
 {
+    int Max_Attempts = 5, q = 0;
     int start = 1;
     int finish = 100;
     setlocale(LC_CTYPE, "Russian");
     int number = generate_number(start, finish);
 
+    cout << "У вас " << Max_Attempts << " попыток, чтобы угадать число\n";
     int a;
     do
     {
-        cout << "Введите число: ";
+        cout << q + 1 << " попытка. Введите число: ";
         cin >> a;
         if (a < number) cout << "Вы ввели число, меньшее загаданного. Попробуйте еще раз\n";
         else if (a > number) cout << "Вы ввели число, большее загаданного. Попробуйте еще раз\n";
         else {cout << "Вы угадали!\n"; break;}
-    } while(true);
-
+        q++;
+    } while(q < Max_Attempts);
+    if (q == Max_Attempts)
+        cout << "Вы исчерпали все попытки. Вы проиграли :( Правильный ответ был " << number << "\n";
     return 0;
 }
 
